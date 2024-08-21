49e9438f9b087077b3e144fc3d15b13beb35dbd9
sweta927 <swetashah927@gmail.com>
2024-08-21 17:04:04 +0530

    intialized react app and added sidebar panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DataView from "./components/DataView/DataView";
+import Sidebar from "./components/Sidebar/Sidebar";
+import PivotView from "./components/PivotView/PivotView";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<DataView />} />
+        <Route path="/pivotTable" element={<PivotView />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
